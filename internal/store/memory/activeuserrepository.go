0599707f91c932f63131cce4241f3647d8a2e36f
Novikov Dmitriy <dn@aube.work>
2025-05-08 12:12:21 +0300

    post-review changes

--- internal/store/memory/activeuserrepository.go
+++ internal/store/memory/activeuserrepository.go
@@ -2,23 +2,32 @@ package memory
 
 import (
 	"context"
+	"sync"
 
 	"github.com/aube/gophermart/internal/model"
 )
 
 // ActiveUserRepository ...
 type ActiveUserRepository struct {
+	mu  sync.Mutex
 	mem map[string]*model.User
 }
 
 // Set ...
 func (r *ActiveUserRepository) Set(ctx context.Context, u *model.User) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
 	r.mem[u.RandomHash] = u
+
 	return nil
 }
 
 // Get ...
 func (r *ActiveUserRepository) Get(ctx context.Context, RandomHash string) (*model.User, bool) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
 	user, ok := r.mem[RandomHash]
 
 	return user, ok
